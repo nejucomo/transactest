288a53a7af17ecb7d0d31cf97307b6d8576addcc
Nathan Wilcox <nejucomo@gmail.com>
2015-03-24 20:37:28 -0700

    Incomplete first-draft of transactest:
    transactest takes a specification similar to
    https://github.com/ethereum/tests/StateTests, except:
    
    1. Only a single test is specified per file.
    2. There is no full-test "post" assertion, instead:
    3. Each transactions is associated with any number of optional
       post-transaction assertions (such as account state assertions), and
    4. There may be any number of transactions in a given test specification.
    5. Test runs are deterministic (with a CSPRNG seed).
    6. Rather than requiring literal values for code, addresses, secret
       keys, etc... the specification accepts programmer-friendly field
       specifications, such as "alice" as an abstraction over a keypair or
       "sourcefile:../src/mycode.se" for automatic compilation.
    
    This is a Go implementation which depends on go-ethereum and is based
    off of reading ethtest. Ideally there would be a test runner tool for
    each client implementation for very cautious contract developers /
    auditors.

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"io"
+	"os"
+	"log"
+	"io/ioutil"
+	"encoding/json"
+)
+
+
+func main() {
+	if len(os.Args) > 1 {
+		runPath(os.Args[1])
+	} else {
+		runReader(os.Stdin)
+	}
+}
+
+func runPath(p string) {
+	f, err := os.Open(p)
+	checkErr(err)
+	runReader(f)
+}
+
+func runReader(r io.Reader) {
+	data, err := ioutil.ReadAll(r)
+	checkErr(err)
+	runBytes(data)
+}
+
+func runBytes(data []byte) {
+	var spec TestSpec
+
+	err := json.Unmarshal(data, &spec)
+	checkErr(err)
+
+	runTestSpec(spec)
+}
+
+func runTestSpec(spec TestSpec) {
+	log.Fatalf("Not implemented.\n")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+type TestSpec struct {
+	transactions []TransactionAssertions
+}
+
+type TransactionAssertions struct {
+	Transaction
+	Assertions
+}
+
+type Transaction struct {
+	data []byte
+	gasLimit Gas
+	gasPrice Gas
+	nonce SeqNum
+	sender AccountId
+	to AccountId
+	value Ether
+}
+
+type Gas uint
+type SeqNum uint
+type AccountId uint
+type Ether uint
+
+type Assertions struct {
+	accounts map[AccountId]AccountAssertion
+}
+
+type AccountAssertion struct {
+	balance Ether
+	code ByteCode
+	nonce SeqNum
+	storage StorageAssertion
+}
+
+type ByteCode []byte
+
+type StorageAssertion struct {
+	data map[string]string
+}
